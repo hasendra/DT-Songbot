f783a06927b9b59ceee711f439f9491245abf2e2
hasendra <72311160+hasendra@users.noreply.github.com>
2021-06-26 14:58:00 +0530

    Update songs.py

--- DTSongBot/plugins/songs.py
+++ DTSongBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '🎵 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝙗𝙮 @TheAnkiVectorbot•••\n 𝙟𝙤𝙞𝙣 @ankivectorUpdates •••'
+        rep = '🎵 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝙗𝙮 ♔〘Ł€Ꮆ€ŇĐ〙♔ •••\n 𝙟𝙤𝙞𝙣 @Legend000001 •••'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -62,7 +62,7 @@ def song(client, message):
         s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur, performer=str(yt.author))
         m.delete()
     except Exception as e:
-        m.edit('❌ Error\n Report @AnkiSupport_Official')
+        m.edit('❌ Error\n Report @publictechno')
         print(e)
 
     try:
